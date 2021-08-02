1df2e498e8e9d527bdb3155b3492a08ff7f8e7c0
Minsang <sunflower_94@kaist.ac.kr>
2021-08-02 16:17:30 +0900

    config 수정

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
-# mmdetection-segmentation settings
-MODEL_SEG_CONFIG_FILE = 'mmdetection/configs/yolact/yolact_r101_1x8_coco.py'
-MODEL_SEG_CHECKPOINT_FILE = 'mmdetection/checkpoints/yolact_r101_1x8_coco_20200908-4cbe9101.pth'
-MODEL_EDGE_CHECKPOINT_FILE = 'edgeconnect/checkpoints/places2/EdgeModel_gen.pth'
-MODEL_INPAINT_CHECKPOINT_FILE = 'edgeconnect//checkpoints/places2/InpaintingModel_gen.pth'
+FRAME_RATE = 0.5
 DEVICE = 'cpu'
-
-# cv2 settings
-WIDTH = 128
-HEIGHT = 128
+RESIZE = 256
+TARGET_CLASS = 15
+MODEL_EDGE_CHECKPOINT_PATH = 'edgeconnect/checkpoints/places2/EdgeModel_gen.pth'
+MODEL_INPAINT_CHECKPOINT_PATH = 'edgeconnect/checkpoints/places2/InpaintingModel_gen.pth'
