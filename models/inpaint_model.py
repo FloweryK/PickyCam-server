c305b9455ce2d997923c9db9663747290e8e2391
Minsang <sunflower_94@kaist.ac.kr>
2021-08-02 16:19:14 +0900

    SegModel, InpaintModel 분리

---
+++ models/inpaint_model.py
@@ -0,0 +1,71 @@
+import torch
+import numpy as np
+from PIL import Image
+from torchvision import models
+from torchvision import transforms as T
+from skimage.color import rgb2gray
+from skimage.feature import canny
+from edgeconnect.src.networks import EdgeGenerator, InpaintGenerator
+
+
+class InpaintModel:
+	def __init__(self, device, edge_checkpoint, inpaint_checkpoint):
+		self.device = device
+
+		self.edge_model = EdgeGenerator()
+		self.edge_model.load_state_dict(torch.load(edge_checkpoint, map_location=torch.device(device))['generator'])
+		self.edge_model = self.edge_model.eval()
+		self.edge_model = self.edge_model.to(device)
+
+		self.inpaint_model = InpaintGenerator()
+		self.inpaint_model.load_state_dict(torch.load(inpaint_checkpoint, map_location=torch.device(device))['generator'])
+		self.inpaint_model = self.inpaint_model.eval()
+		self.inpaint_model = self.inpaint_model.to(device)
+
+	@staticmethod
+	def preprocess(img, grayscale=False):
+		if grayscale:
+			t = T.Compose([
+				T.Grayscale(),
+				T.ToTensor()
+			])
+		else:
+			t = T.Compose([
+				T.ToTensor()
+			])
+
+		img = Image.fromarray(img)
+		img = t(img)
+		img = img.float()
+		img = img.unsqueeze(0)
+		return img
+
+	def predict(self, img, mask):
+		# preprocess of edge generation
+		img_gray = self.preprocess(img, grayscale=True)
+		edge = self.preprocess(canny(rgb2gray(img), sigma=1, mask=(1-mask).astype(bool)))
+		mask = self.preprocess(mask)
+
+		img_gray_masked = img_gray * (1 - mask.bool().int()) + mask * 255
+		edge_masked = edge * (1-mask.bool().int())
+		inp_edge = torch.cat((img_gray_masked, edge_masked, mask), dim=1)
+
+		# move input to device
+		inp_edge = inp_edge.to(self.device)
+
+		# generate edges
+		edge_generated = self.edge_model(inp_edge)
+
+		# preprocess of inpainting
+		img = self.preprocess(img)
+		img_masked = img * (1 - mask.bool().int()) + mask * 255
+		img_masked = img_masked.to(self.device)
+		inp_inpaint = torch.cat((img_masked, edge_generated), dim=1)
+
+		# generate paint
+		paint_generated = self.inpaint_model(inp_inpaint)
+
+		# postprocess
+		paint_generated = paint_generated.detach().cpu().numpy()[0]
+
+		return paint_generated
---
+++ models/seg_model.py
@@ -0,0 +1,56 @@
+import torch
+import numpy as np
+from torchvision import models
+from torchvision import transforms as T
+
+
+class SegModel:
+	def __init__(self, device):
+		self.device = device
+
+		# torch/hub.py line 127 and 423 had been modified due to ssl error.
+		# please check: https://gentlesark.tistory.com/57
+		# self.model = torch.hub.load('pytorch/vision', 'fcn_resnet101', pretrained=True)
+		self.model = models.segmentation.deeplabv3_resnet50(pretrained=True)
+		self.model = self.model.eval()
+		self.model = self.model.to(device)
+
+	@staticmethod
+	def preprocess(img):
+		t = T.Compose([
+			T.ToTensor(),
+			T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+		])
+
+		img = t(img)
+		img = img.unsqueeze(0)
+		return img
+
+	def predict(self, img):
+		# preprocess
+		inp = self.preprocess(img)
+
+		# move input to device
+		inp = inp.to(self.device)
+
+		# predict
+		masks = self.model(inp)['out']
+
+		# postprocess
+		mask = torch.argmax(masks.squeeze(), dim=0).detach().cpu().numpy()
+		mask = mask == 15
+
+		# some weird error
+		mask = mask.astype(np.uint8)
+
+		# padd masked area
+		pad = 7
+		mask_right = np.roll(mask, pad, axis=1)
+		mask_left = np.roll(mask, -pad, axis=1)
+		mask_up = np.roll(mask, pad, axis=0)
+		mask_down = np.roll(mask, -pad, axis=0)
+
+		mask = np.logical_or.reduce((mask, mask_up, mask_down, mask_left, mask_right))
+		mask = mask.astype(np.uint8)
+
+		return mask
