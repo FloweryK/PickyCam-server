b68d8baaaaaf9a925ae14ae8345a71d954faba8d
Minsang <sunflower_94@kaist.ac.kr>
2021-08-04 12:41:02 +0900

    checkpoints 경로 이동

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def main():
 	subprocess.call(['mv', 'edge-connect', 'edgeconnect'])
 
 	# you should manually download pre-trained model through google drive: https://drive.google.com/drive/folders/1KyXz4W4SAvfsGh3NJ7XgdOv5t46o-8aa
-	os.makedirs('./edge-connect/checkpoints', exist_ok=True)
+	# os.makedirs('./edge-connect/checkpoints', exist_ok=True)
 
 	# write __init__.py on edgeconnect
 	# with open('./edgeconnect/__init__.py', 'wb'):
