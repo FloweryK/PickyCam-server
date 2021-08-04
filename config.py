469dd5a79f5338b7732aa9e9a3742cff54e6e89d
Minsang <sunflower_94@kaist.ac.kr>
2021-08-04 12:42:40 +0900

    checkpoints 경로 이동

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ DEVICE = 'cpu'
 PAD = 20
 RESIZE = 256
 TARGET_CLASS = 15
-MODEL_EDGE_CHECKPOINT_PATH = 'edgeconnect/checkpoints/places2/EdgeModel_gen.pth'
-MODEL_INPAINT_CHECKPOINT_PATH = 'edgeconnect/checkpoints/places2/InpaintingModel_gen.pth'
+MODEL_EDGE_CHECKPOINT_PATH = 'checkpoints/edgeconnect/places2/EdgeModel_gen.pth'
+MODEL_INPAINT_CHECKPOINT_PATH = 'checkpoints/edgeconnect/places2/InpaintingModel_gen.pth'
