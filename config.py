b273a0d277de859068a385e8e0eb487c6ab0c735
MinsangYu <flowerk94@gmail.com>
2021-10-13 23:42:45 +0900

    minor updates

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+IS_RECORDING = False
 FRAME_RATE = 0.2
 DEVICE = 'cpu'
-PAD = 20
-RESIZE = 256
+PAD = 10
+RESIZE_WIDTH = 128
 TARGET_CLASS = 15
 MODEL_EDGE_CHECKPOINT_PATH = 'checkpoints/places2/EdgeModel_gen.pth'
 MODEL_INPAINT_CHECKPOINT_PATH = 'checkpoints/places2/InpaintingModel_gen.pth'
