94024d0049ffb094b3cbeaeeab15d6fa74851a52
Minsang Yu <flowerk94@gmail.com>
2021-11-20 22:30:23 +0900

    serving changed to class

--- main.py
+++ main.py
@@ -13,19 +13,158 @@ def pad(mask, pad=3):
     return mask
 
 
+def fill_mask_hole(mask):
+    contour, _ = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contour:
+        cv2.drawContours(mask, [cnt], 0, 255, -1)
+    mask = (mask > 0).astype(np.uint8)
+    return mask
+
+
+def replace_masked_area(img1, img2, mask):
+    result = img1.copy()  # deepcopy doesnt work if you directly use img1
+    result[mask == True] = img2[mask == True]
+    return result
+
+
+def overlay_mask(img, mask):
+    result = img.copy()  # deepcopy doesnt work if you directly use img
+    mask_color = np.zeros(result.shape, result.dtype)
+    mask_color[:, :] = (0, 255, 0)
+    mask_color = cv2.bitwise_and(mask_color, mask_color, mask=mask)
+    cv2.addWeighted(mask_color, 0.7, result, 1, 0, result)
+    return result
+
+
+def merge_4by4(img11, img12, img21, img22, width):
+    result = np.vstack(
+        (
+            np.hstack((img11, img12)),
+            np.hstack((img21, img22)),
+        )
+    )
+    result = cv2.resize(result, (width, int(width * result.shape[0] / result.shape[1])))
+    return result
+
+
+def write_text_on_image(img, text):
+    # write fps info
+    for i, line in enumerate(text.split("\n")):
+        pos = (5, 15 + i * 23)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.7
+        color = (0, 0, 0)  # black
+        thickness = 2
+
+        img = cv2.putText(img, line, pos, font, fontScale, color, thickness)
+
+    return img
+
+
+class ServeModel:
+    def __init__(self):
+        # Process Inferencers
+        # TODO: leave out direct configs
+        DEVICE = "cuda"
+
+        # TODO: split network and pre/post process codes in each inferencers
+        self.model_seg = SegModel(DEVICE)
+        self.model_inp = InpaintModel(DEVICE)
+
+        # utils
+        self.timer = Timer()
+
+    def human_segmentation(self, img):
+        masks = self.model_seg(img)
+        if type(masks) != type(None):  # TODO: code cleaning
+            masks = masks.detach().cpu().numpy()
+        else:
+            masks = []
+        return masks
+
+    def face_recognition(self, img, masks):
+        # config
+        PAD = 15
+
+        mask_unknown = [np.zeros(img.shape[:2], dtype=np.float32)]
+        mask_known = [np.zeros(img.shape[:2], dtype=np.float32)]
+
+        for i, mask in enumerate(masks):
+            # TODO: face recognition
+            known = True if i == 0 else False
+
+            if known:
+                mask_known.append(mask)
+            else:
+                mask_unknown.append(mask)
+
+        mask_unknown = sum(mask_unknown)
+        mask_known = sum(mask_known)
+
+        mask_unknown = pad(mask_unknown, PAD)
+        mask_unknown -= mask_known * 100
+        mask_unknown = mask_unknown > 0
+        mask_unknown = mask_unknown.astype(np.uint8)
+        mask_unknown = fill_mask_hole(mask_unknown)
+
+        return mask_unknown
+
+    def inpaint(self, img, mask, resize):
+        # preprocess
+        img = cv2.resize(img, resize, interpolation=cv2.INTER_AREA)
+        mask = cv2.resize(mask, resize, interpolation=cv2.INTER_AREA)
+
+        # net forward
+        img = self.model_inp(img, mask)
+        return img
+
+    def inference(self, img):
+        RESIZE_ORG = (480, 1016)
+        RESIZE_INP = (108, 192)
+
+        # timer
+        self.timer.initialize()
+
+        # convert from BGR to RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # human segmantation
+        masks = self.human_segmentation(img)
+        self.timer.check("human segmentation")
+
+        # known face recognition
+        mask = self.face_recognition(img, masks)
+        self.timer.check("known face recognition")
+
+        # inpainting
+        img_inp = self.inpaint(img, mask, RESIZE_INP)
+        img_inp = img_inp.detach().cpu().numpy()
+        img_inp = cv2.convertScaleAbs(img_inp, alpha=(255.0))
+        self.timer.check("inpainting")
+
+        # resize to original size
+        mask = cv2.resize(mask, RESIZE_ORG, interpolation=cv2.INTER_NEAREST)
+        img_inp = cv2.resize(img_inp, RESIZE_ORG, interpolation=cv2.INTER_CUBIC)
+
+        # replace human into inpainted background
+        img_erased = replace_masked_area(img, img_inp, mask)
+
+        # make img with mask color
+        img_mask = overlay_mask(img, mask)
+
+        # tetris display
+        result = merge_4by4(img, img_mask, img_inp, img_erased, width=500)
+        result = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
+        self.timer.check("merging")
+
+        # write fps info
+        result = write_text_on_image(result, self.timer.get_result_as_text())
+
+        return result
+
+
 def main():
-    # settings
-    DEVICE = "cuda"
-    RESIZE_ORG = (480, 1016)
-    RESIZE_INP = (108, 192)
-    RESIZE_SPR = (120, 254)
-    PAD = 15
-
-    # timer
-    timer = Timer()
-
-    model_seg = SegModel(DEVICE)
-    model_inpaint = InpaintModel(DEVICE)
+    serve_model = ServeModel()
 
     cap = cv2.VideoCapture("testvideo2.mp4")
 
@@ -34,90 +173,12 @@ def main():
         success, img = cap.read()
 
         if success:
-            timer.initialize()
-
-            # convert from BGR to RGB
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            # human segmantation
-            masks = model_seg(img)
-            if type(masks) != type(None): # TODO: code cleaning
-                masks = masks.detach().cpu().numpy()
-            else:
-                masks = []
-            timer.check("human segmentation")
-            
-            # known face recognition
-            mask_unknown = [np.zeros(img.shape[:2], dtype=np.float32)]
-            mask_known = [np.zeros(img.shape[:2], dtype=np.float32)]
-
-            for i, mask in enumerate(masks):
-                # TODO: face recognition
-                known = True if i == 0 else False
-
-                if known:
-                    mask_known.append(mask)
-                else:
-                    mask_unknown.append(mask)
-            
-            mask_unknown = sum(mask_unknown)
-            mask_known = sum(mask_known)
-
-            mask_unknown = pad(mask_unknown, PAD)
-            mask_unknown -= mask_known * 100
-            mask_unknown = (mask_unknown > 0)
-            mask_unknown = mask_unknown.astype(np.uint8)
-
-            # fill holes inside each mask
-            contour, _ = cv2.findContours(mask_unknown, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-            for cnt in contour:
-                cv2.drawContours(mask_unknown, [cnt], 0, 255, -1)
-            mask_unknown = (mask_unknown > 0)
-            mask_unknown = mask_unknown.astype(np.uint8)
-            timer.check("known face recognition")
-
-            # inpainting
-            img_inpaint = cv2.resize(img, RESIZE_INP, interpolation=cv2.INTER_AREA)
-            mask_inpaint = cv2.resize(mask_unknown, RESIZE_INP, interpolation=cv2.INTER_AREA)
-
-            img_inpaint = model_inpaint(img_inpaint, mask_inpaint)
-            img_inpaint = img_inpaint.detach().cpu().numpy()
-
-            img_inpaint = cv2.convertScaleAbs(img_inpaint, alpha=(255.0))
-            timer.check("inpainting")
-
-            # resize to original size
-            mask_unknown = cv2.resize(mask_unknown, RESIZE_ORG, interpolation=cv2.INTER_NEAREST)
-            img_inpaint = cv2.resize(img_inpaint, RESIZE_ORG, interpolation=cv2.INTER_CUBIC)
-
-            # replace human into inpainted background
-            img_erased = img.copy()
-            img_erased[mask_unknown == True] = img_inpaint[mask_unknown == True]
-
-            # tetris display
-            img_with_mask = img.copy()
-            mask_color = np.zeros(img_with_mask.shape, img_with_mask.dtype)
-            mask_color[:, :] = (0, 255, 0)
-            mask_color = cv2.bitwise_and(mask_color, mask_color, mask=mask_unknown)
-            cv2.addWeighted(mask_color, 0.7, img_with_mask, 1, 0, img_with_mask)
-
-            display = np.vstack((
-                np.hstack((img, img_with_mask)), 
-                np.hstack((img_inpaint, img_erased))
-                ))
-            display = cv2.resize(display, (500, int(500 * display.shape[0]/display.shape[1])))
-            display = cv2.cvtColor(display, cv2.COLOR_RGB2BGR)
-            timer.check("merging")
-
-            # write fps info
-            for i, line in enumerate(timer.get_result_as_text().split("\n")):
-                display = cv2.putText(display, line, (5, 15 + i * 23), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2,)
+            result = serve_model.inference(img)
 
             # show result
-            cv2.imshow("result", display)
+            cv2.imshow("result", result)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
-
 
 
 if __name__ == "__main__":
