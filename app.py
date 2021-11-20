cce2d7bcbc291cf192a7194bf30ca4a9b25cce53
Minsang Yu <flowerk94@gmail.com>
2021-11-21 00:53:41 +0900

    socket 통신 server, test_client 구현

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask, request, jsonify
-from server.inference import Inferencer
-from PIL import Image
-import numpy as np
-import cv2
-import io
 import base64
-import time
-import ssl
+import cv2
+import numpy as np
+from flask import Flask
+from flask_socketio import SocketIO
+from serve_model import ServeModel
+
+
+def base64_to_img(string):
+    buffer = base64.b64decode(string)
+    img_np = np.frombuffer(buffer, dtype=np.uint8)
+    img = cv2.imdecode(img_np, flags=1)
+    return img
+
+
+def img_to_base64(img):
+    _, buffer = cv2.imencode(".jpg", img)
+    string = base64.b64encode(buffer)
+    return string
+
 
 # server config
 HOST = "localhost"
-PORT = 4321
-
-# inference model
-model = Inferencer()
+PORT = 8000
 
 # define server
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "test"
+socketio = SocketIO(app)
+
+# serve model
+serve_model = ServeModel()
 
 
 @app.route("/")
@@ -24,29 +37,35 @@ def home():
     return "hello world!"
 
 
-@app.route("/upload", methods=["POST"])
-def upload():
-    # decode base64 image
-    img = request.json["data"]
-    print("got data from client!: ", len(img))
-    img = base64.b64decode(img)
-    img = np.frombuffer(img, dtype=np.uint8)
-    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+@socketio.on("connect")
+def on_connect():
+    print("client connected, waiting for frames")
 
-    # inference
-    img = model.inference(img)
-    img = Image.fromarray(img.astype("uint8"))
 
-    # encode image to base64
-    rawBytes = io.BytesIO()
-    img.save(rawBytes, "JPEG")
-    rawBytes.seek(0)
-    img = base64.b64encode(rawBytes.read())
-    print("sent: ", len(img))
+@socketio.on("disconnect")
+def on_disconnect():
+    print("client disconnected.")
 
-    return jsonify({"image": str(img)})
+
+@socketio.on("upload")
+def upload(data):
+    print("got frame from client: ", data["frame"][-10:])
+
+    # read data
+    string = data["frame"]
+
+    # convert from base64 to cv2 format
+    img = base64_to_img(string)
+
+    # process image
+    img_processed = serve_model.inference(img)
+
+    # convert from cv2 format to base64
+    string_processed = img_to_base64(img_processed)
+
+    # response
+    socketio.emit("response", {"processed": string_processed})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host=HOST, port=PORT)
+    socketio.run(app, host=HOST, port=PORT, debug=True)
---
+++ test_client.py
@@ -0,0 +1,69 @@
+import base64
+import socketio
+import cv2
+import numpy as np
+
+
+def img_to_base64(img):
+    _, buffer = cv2.imencode(".jpg", img)
+    string = base64.b64encode(buffer)
+    return string
+
+
+def base64_to_img(string):
+    buffer = base64.b64decode(string)
+    img_np = np.frombuffer(buffer, dtype=np.uint8)
+    img = cv2.imdecode(img_np, flags=1)
+    return img
+
+
+# client config
+HOST = "localhost"
+PORT = 8000
+
+# define client
+sio = socketio.Client()
+
+
+@sio.event
+def connect():
+    print("server connected, sending frames")
+
+
+@sio.event
+def disconnect():
+    print("server disconnected.")
+
+
+@sio.event
+def response(data):
+    print("got response from server: ", data["processed"][-10:])
+
+    # read data
+    string = data["processed"]
+
+    # convert from base64 to cv2 format
+    img = base64_to_img(string)
+
+    # show image
+    while True:
+        cv2.imshow("result", img)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+
+# start connection
+sio.connect("http://localhost:8000")
+
+# emit event
+cap = cv2.VideoCapture("testvideo2.mp4")
+while cap.isOpened():
+    success, img = cap.read()
+
+    if success:
+        string = img_to_base64(img)
+        sio.call("upload", {"frame": string})
+
+
+# disconnect
+sio.disconnect()
