6d1e1615968faf522f414d996d1e60bb7ea5b3ee
FloweryK <flowerk94@gmail.com>
2022-01-11 15:10:40 +0900

    [feat] util 함수 분리 (utils/images.py), 옵션 추가, face recognition 추가

--- serve_model.py
+++ serve_model.py
@@ -1,75 +1,12 @@
+import os
 import cv2
+from face_recognition.api import face_distance, face_encodings, face_locations
 import numpy as np
+import face_recognition as fr
 from models.segmentation.yolact.model import SegModel
 from models.inpainting.edgeconnect.model import InpaintModel
 from utils.timer import Timer
-
-
-def cal_shape(shape, w_target, by4=False):
-    w, h = shape
-    h_target = int(h * (w_target / w))
-
-    if by4:
-        r = h_target % 4
-        h_target += (4 - r) if r > 2 else (-r)
-
-    return (w_target, h_target)
-
-
-def padding(mask, pad=3):
-    # pad masked area
-    kernel = np.ones((1 + 2 * pad, 1 + 2 * pad))
-    mask = cv2.filter2D(mask.astype(np.float32), -1, kernel)
-    mask = (mask >= 1).astype(np.float32)
-    return mask
-
-
-def fill_mask_hole(mask):
-    contour, _ = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contour:
-        cv2.drawContours(mask, [cnt], 0, 255, -1)
-    mask = (mask > 0).astype(np.uint8)
-    return mask
-
-
-def replace_masked_area(img1, img2, mask):
-    result = img1.copy()  # deepcopy doesnt work if you directly use img1
-    result[mask == True] = img2[mask == True]
-    return result
-
-
-def overlay_mask(img, mask):
-    result = img.copy()  # deepcopy doesnt work if you directly use img
-    mask_color = np.zeros(result.shape, result.dtype)
-    mask_color[:, :] = (0, 255, 0)
-    mask_color = cv2.bitwise_and(mask_color, mask_color, mask=mask)
-    cv2.addWeighted(mask_color, 0.7, result, 1, 0, result)
-    return result
-
-
-def merge_4by4(img11, img12, img21, img22, width):
-    result = np.vstack(
-        (
-            np.hstack((img11, img12)),
-            np.hstack((img21, img22)),
-        )
-    )
-    result = cv2.resize(result, (width, int(width * result.shape[0] / result.shape[1])))
-    return result
-
-
-def write_text_on_image(img, text):
-    # write fps info
-    for i, line in enumerate(text.split("\n")):
-        pos = (5, 15 + i * 23)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        fontScale = 0.7
-        color = (0, 0, 0)  # black
-        thickness = 2
-
-        img = cv2.putText(img, line, pos, font, fontScale, color, thickness)
-
-    return img
+from utils.images import *
 
 
 class ServeModel:
@@ -81,8 +18,19 @@ class ServeModel:
         self.model_seg = SegModel(DEVICE)
         self.model_inp = InpaintModel(DEVICE)
 
+        # load known_faces
+        self.known_faces = []
+        self.load_known_faces()
+
         # utils
         self.timer = Timer()
+
+    def load_known_faces(self, dir='faces/'):
+        for file_name in os.listdir(dir):
+            img = fr.load_image_file(os.path.join(dir, file_name))
+            face_locations = fr.face_locations(img)
+            face_encodings = fr.face_encodings(img, face_locations)
+            self.known_faces += face_encodings
 
     def human_segmentation(self, img, shape):
         # preprocess
@@ -99,34 +47,38 @@ class ServeModel:
 
         return masks
 
-    def face_recognition(self, img, masks, shape, pad_ratio):
+    def face_recognition(self, img, masks, shape):
+        knowns = [False for _ in range(len(masks))]
+
         # preprocess
         img = cv2.resize(img, shape, interpolation=cv2.INTER_AREA)
 
-        mask_unknown = [np.zeros(img.shape[:2], dtype=np.float32)]
-        mask_known = [np.zeros(img.shape[:2], dtype=np.float32)]
+        # extract all faces
+        face_locations = fr.face_locations(img)
+        face_encodings = fr.face_encodings(img, face_locations)
 
-        for i, mask in enumerate(masks):
-            # TODO: face recognition
-            # known = True if i == 0 else False
-            known = False
+        # # match face locations to mask
+        if len(masks) > 0:
+            face_to_mask = {}
+            shape_mask = masks[0].shape[::-1]
+            r = (shape_mask[0] / shape[0])
 
-            if known:
-                mask_known.append(mask)
-            else:
-                mask_unknown.append(mask)
+            for i, location in enumerate(face_locations):
+                ymin, xmax, ymax, xmin = (np.array(location) * r).astype(int).tolist()
 
-        mask_unknown = sum(mask_unknown)
-        mask_known = sum(mask_known)
+                areas = [np.sum(mask[xmin:xmax, ymin:ymax]) for mask in masks]
+                face_to_mask[i] = np.argmax(areas)
 
-        # postprocess
-        mask_unknown = padding(mask_unknown, pad=int(pad_ratio * img.shape[1]))
-        mask_unknown -= mask_known * 100
-        mask_unknown = mask_unknown > 0
-        mask_unknown = mask_unknown.astype(np.uint8)
-        mask_unknown = fill_mask_hole(mask_unknown)
+            # find known face
+            for i, encoding in enumerate(face_encodings):
+                face_distances = fr.face_distance(self.known_faces, encoding)
+                
+                known = (np.array(face_distances) < 0.5).any()
 
-        return mask_unknown
+                if known:
+                    knowns[face_to_mask[i]] = True
+
+        return knowns
 
     def inpaint(self, img, mask, shape):
         # preprocess
@@ -139,19 +91,24 @@ class ServeModel:
 
     def inference(self, img, options={
         "width_seg": 480,
+        "width_fcr": 240,
         "width_inp": 100,
-        "pad_ratio": 0.04,
+        "pad_ratio_known": 0.01,
+        "pad_ratio_unknown": 0.04,
         "isDebug": False
     }):
         # config
         WIDTH_SEG = options["width_seg"]
+        WIDTH_FCR = options["width_fcr"]
         WIDTH_INP = options["width_inp"]
-        PAD_RATIO = options["pad_ratio"]
+        PAD_RATIO_KNOWN = options["pad_ratio_known"]
+        PAD_RATIO_UNKNOWN = options["pad_ratio_unknown"]
         IS_DEBUG = options["isDebug"]
 
         # settings
         shape_org = img.shape[:2][::-1]
-        shape_seg = cal_shape(shape_org, w_target=WIDTH_SEG, by4=False)
+        shape_seg = cal_shape(shape_org, w_target=WIDTH_SEG)
+        shape_fcr = cal_shape(shape_org, w_target=WIDTH_FCR)
         shape_inp = cal_shape(shape_org, w_target=WIDTH_INP, by4=True)
 
         # timer
@@ -166,8 +123,29 @@ class ServeModel:
         self.timer.check("human segmentation")
 
         # known face recognition
-        mask = self.face_recognition(img, masks, shape_seg, PAD_RATIO)
+        knowns = self.face_recognition(img, masks, shape_fcr)
         self.timer.check("known face recognition")
+
+        # post process 
+        mask_unknown = [np.zeros(shape_seg[::-1], dtype=np.float32)]
+        mask_known = [np.zeros(shape_seg[::-1], dtype=np.float32)]
+        for i, known in enumerate(knowns):
+            if known:
+                mask_known.append(masks[i])
+            else:
+                mask_unknown.append(masks[i])
+
+        mask_unknown = sum(mask_unknown)
+        mask_known = sum(mask_known)
+
+        mask_unknown = padding(mask_unknown, pad=int(PAD_RATIO_UNKNOWN * img.shape[1]))
+        mask_known = padding(mask_known, pad=int(PAD_RATIO_KNOWN * img.shape[1]))
+        mask_unknown -= mask_known * 100
+        mask_unknown = mask_unknown > 0
+        mask_unknown = mask_unknown.astype(np.uint8)
+        mask_unknown = fill_mask_hole(mask_unknown)
+        mask = mask_unknown
+        self.timer.check("unknown mask merging")
 
         # inpainting
         img_inp = self.inpaint(img, mask, shape_inp)
@@ -191,7 +169,7 @@ class ServeModel:
         if IS_DEBUG:
             result = merge_4by4(img, img_mask, img_inp, img_erased, width=500)
         else:
-            result = img_erased
+            result = img
         result = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
         self.timer.check("dev merging")
 
@@ -208,9 +186,31 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--video", required=True, type=str)
+    parser.add_argument("--debug", default=True, type=bool)
+    parser.add_argument("--width_seg", default=480, type=int)
+    parser.add_argument("--width_fcr", default=480, type=int)
+    parser.add_argument("--width_inp", default=100, type=int)
+    parser.add_argument("--pad_ratio_known", default=0.01, type=float)
+    parser.add_argument("--pad_ratio_unknown", default=0.04, type=float)
+
     args = parser.parse_args()
 
     video_path = args.video
+    isDebug = args.debug
+    width_seg = args.width_seg
+    width_fcr = args.width_fcr
+    width_inp = args.width_inp
+    pad_ratio_known = args.pad_ratio_known
+    pad_ratio_unknown = args.pad_ratio_unknown
+
+    options = {
+        "width_seg": width_seg,
+        "width_inp": width_inp,
+        "width_fcr": width_fcr,
+        "pad_ratio_known": pad_ratio_known,
+        "pad_ratio_unknown": pad_ratio_unknown,
+        "isDebug": isDebug
+    }
 
     serve_model = ServeModel()
 
@@ -221,7 +221,7 @@ if __name__ == "__main__":
         success, img = cap.read()
 
         if success:
-            result = serve_model.inference(img)
+            result = serve_model.inference(img, options=options)
 
             # show result
             cv2.imshow("result", result)
---
+++ utils/images.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+
+def cal_shape(shape, w_target, by4=False):
+    w, h = shape
+    h_target = int(h * (w_target / w))
+
+    if by4:
+        r = h_target % 4
+        h_target += (4 - r) if r > 2 else (-r)
+
+    return (w_target, h_target)
+
+
+def padding(mask, pad=3):
+    # pad masked area
+    kernel = np.ones((1 + 2 * pad, 1 + 2 * pad))
+    mask = cv2.filter2D(mask.astype(np.float32), -1, kernel)
+    mask = (mask >= 1).astype(np.float32)
+    return mask
+
+
+def fill_mask_hole(mask):
+    contour, _ = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contour:
+        cv2.drawContours(mask, [cnt], 0, 255, -1)
+    mask = (mask > 0).astype(np.uint8)
+    return mask
+
+
+def replace_masked_area(img1, img2, mask):
+    result = img1.copy()  # deepcopy doesnt work if you directly use img1
+    result[mask == True] = img2[mask == True]
+    return result
+
+
+def overlay_mask(img, mask):
+    result = img.copy()  # deepcopy doesnt work if you directly use img
+    mask_color = np.zeros(result.shape, result.dtype)
+    mask_color[:, :] = (0, 255, 0)
+    mask_color = cv2.bitwise_and(mask_color, mask_color, mask=mask)
+    cv2.addWeighted(mask_color, 0.7, result, 1, 0, result)
+    return result
+
+
+def merge_4by4(img11, img12, img21, img22, width):
+    result = np.vstack(
+        (
+            np.hstack((img11, img12)),
+            np.hstack((img21, img22)),
+        )
+    )
+    result = cv2.resize(result, (width, int(width * result.shape[0] / result.shape[1])))
+    return result
+
+
+def write_text_on_image(img, text):
+    # write fps info
+    for i, line in enumerate(text.split("\n")):
+        pos = (5, 15 + i * 23)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.7
+        color = (0, 0, 0)  # black
+        thickness = 2
+
+        img = cv2.putText(img, line, pos, font, fontScale, color, thickness)
+
+    return img
